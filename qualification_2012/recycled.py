983a53ba0e4d57af15ae93a64e2830c15f3f4e6c
Alex Cucu <alexandruioan.cucu@gmail.com>
2012-10-09 19:12:12 +0300

    Use bitarray instead of set to reduce memory usage

--- qualification_2012/recycled.py
+++ qualification_2012/recycled.py
@@ -1,25 +1,31 @@
-import copy
+import bitarray
+import math
 import sys
 
 
 def circular_permutation(number):
-    for _ in xrange(len(number)):
-        number = '%s%s' % (number[-1], number[:-1])
-        yield number
+    no_digits = int(math.log10(number)) + 1
+    for _ in xrange(no_digits):
+        last_digit = number % 10
+        number = last_digit * 10 ** (no_digits - 1) + number / 10
+        yield int(number)
         
 
 
 def recycle(lower, upper):
     result = 0
-    numbers_set = set(str(x) for x in xrange(lower, upper + 1))
-    for number in copy.copy(numbers_set):
-        if number not in numbers_set:
+    already_parsed = bitarray.bitarray(upper - lower + 1)
+    already_parsed.setall(0)
+    for number in xrange(lower, upper + 1):
+        if already_parsed[number - lower]:
             continue
         permutation_no = 0
         for permutation in circular_permutation(number):
-            if permutation in numbers_set:
+            if permutation < lower or permutation > upper:
+                continue
+            if not already_parsed[permutation - lower]:
                 permutation_no += 1
-                numbers_set.remove(permutation)
+                already_parsed[permutation - lower] = True
         result += permutation_no * (permutation_no - 1) / 2
     return result
 
@@ -36,10 +42,10 @@ def parse_file(input_file):
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         sys.exit(1)
-    parse_file(sys.argv[1])
+    # parse_file(sys.argv[1])
 
-    # import cProfile
-    # def run():
-    #     parse_file(sys.argv[1])
+    import cProfile
+    def run():
+        parse_file(sys.argv[1])
 
-    # cProfile.run('run()', 'profile_output')
+    cProfile.run('run()', 'profile_output')
